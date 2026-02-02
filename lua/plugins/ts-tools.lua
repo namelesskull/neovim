a4aafcbe9a108b67e8eabb492357787cd9dc2600
Khaqul Huda <khaqulhudaa@gmail.com>
2026-02-02 13:35:17 +0700

    disable tools ts-tools

--- lua/plugins/ts-tools.lua
+++ lua/plugins/ts-tools.lua
@@ -1,21 +1,22 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {
-    settings = {
-      tsserver_file_preferences = {
-        includeInlayParameterNameHints = "all",
-        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-      tsserver_format_options = {
-        allowIncompleteCompletions = false,
-        allowRenameOfImportPath = true,
-      },
-    },
-  },
+  opts = {},
+  -- opts = {
+  --   settings = {
+  --     tsserver_file_preferences = {
+  --       includeInlayParameterNameHints = "all",
+  --       includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+  --       includeInlayFunctionParameterTypeHints = true,
+  --       includeInlayVariableTypeHints = true,
+  --       includeInlayPropertyDeclarationTypeHints = true,
+  --       includeInlayFunctionLikeReturnTypeHints = true,
+  --       includeInlayEnumMemberValueHints = true,
+  --     },
+  --     tsserver_format_options = {
+  --       allowIncompleteCompletions = false,
+  --       allowRenameOfImportPath = true,
+  --     },
+  --   },
+  -- },
 }
